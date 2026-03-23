da4fed879127d411efef7baadc1dcb0b6f80c3af
vanhdilac <vietanh.ngotran@gmail.com>
2026-03-23 17:56:11 +0700

    tailwind fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -12,7 +11,6 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [
         react(),
-        tailwindcss()
       ],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
